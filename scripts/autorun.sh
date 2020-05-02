e836619648ea55ef30cc89a18c659148c45c067b
procleap <procleap@users.noreply.github.com>
2020-05-02 13:45:36 +0200

    Fix spelling

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -10,5 +10,5 @@ function run {
 # Load previously configured wallpaper.
 run nitrogen --restore
 
-# Enable compositor effects
+# Enable compositor effects.
 run picom --config ~/.config/awesome/conf/picom.cfg
