42e8bb4e4bbab47b112866b930f5ae6020e7bb4e
procleap <procleap@users.noreply.github.com>
2020-05-02 13:24:28 +0200

    Run autorun.sh script on startup

--- rc.lua
+++ rc.lua
@@ -548,3 +548,6 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Start up user-defined apps.
+awful.spawn.with_shell("~/.config/awesome/scripts/autorun.sh")
