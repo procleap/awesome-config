4b1d78512cf0745bb61824380623a4ca22725701
procleap <procleap@users.noreply.github.com>
2020-05-02 14:21:59 +0200

    Add space between titlebar buttons

--- rc.lua
+++ rc.lua
@@ -537,7 +537,8 @@ client.connect_signal("request::titlebars", function(c)
             awful.titlebar.widget.maximizedbutton(c),
             awful.titlebar.widget.ontopbutton    (c),
             awful.titlebar.widget.closebutton    (c),
-            layout = wibox.layout.fixed.horizontal()
+            layout = wibox.layout.fixed.horizontal(),
+            spacing = 7,
         },
         layout = wibox.layout.align.horizontal
     }
