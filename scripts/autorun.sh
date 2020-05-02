3bce84be7385c47dbadd7f4da2c250b836d7bce6
procleap <procleap@users.noreply.github.com>
2020-05-02 13:28:12 +0200

    Document code

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+# Check if process is already running before attempting to start it.
 function run {
   if ! pgrep -f $1 ; then
     $@&
   fi
 }
 
-# Load previously configured wallpaper
+# Load previously configured wallpaper.
 run nitrogen --restore
