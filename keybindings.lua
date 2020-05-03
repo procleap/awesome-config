f0d89c3b5a890bc7cb434afd618f3c2b05080a03
procleap <procleap@users.noreply.github.com>
2020-05-03 17:12:51 +0200

    Format code

--- keybindings.lua
+++ keybindings.lua
@@ -8,49 +8,87 @@ keys = {}
 
 keys.global = gears.table.join(
     {
-        awful.key({ mod,           }, "s",      hotkeys_popup.show_help,
-        {description="show help", group="awesome"}),
-        awful.key({ mod,           }, "Left",   awful.tag.viewprev,
-        {description = "view previous", group = "tag"}),
-        awful.key({ mod,           }, "Right",  awful.tag.viewnext,
-        {description = "view next", group = "tag"}),
-        awful.key({ mod,           }, "Escape", awful.tag.history.restore,
-        {description = "go back", group = "tag"}),
-
-        awful.key({ mod,           }, "j",
-        function ()
-        awful.client.focus.byidx( 1)
-        end,
-        {description = "focus next by index", group = "client"}
+        awful.key(
+            {mod}, "s", hotkeys_popup.show_help,
+            {description="show help", group="awesome"}
         ),
-        awful.key({ mod,           }, "k",
-        function ()
-        awful.client.focus.byidx(-1)
-        end,
-        {description = "focus previous by index", group = "client"}
+        awful.key(
+            {mod}, "Left", awful.tag.viewprev,
+            {description = "view previous", group = "tag"}
         ),
-        awful.key({ mod,           }, "w", function () mymainmenu:show() end,
-        {description = "show main menu", group = "awesome"}),
+        awful.key(
+            {mod}, "Right", awful.tag.viewnext,
+            {description = "view next", group = "tag"}
+        ),
+        awful.key(
+            {mod}, "Escape", awful.tag.history.restore,
+            {description = "go back", group = "tag"}
+        ),
+        awful.key(
+            { mod}, "j",
+            function ()
+                awful.client.focus.byidx(1)
+            end,
+            {description = "focus next by index", group = "client"}
+        ),
+        awful.key(
+            {mod}, "k",
+            function ()
+                awful.client.focus.byidx(-1)
+            end,
+            {description = "focus previous by index", group = "client"}
+        ),
+        awful.key(
+            {mod}, "w",
+            function ()
+                mymainmenu:show()
+            end,
+            {description = "show main menu", group = "awesome"}
+        ),
 
         -- Layout manipulation
-        awful.key({ mod, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
-        {description = "swap with next client by index", group = "client"}),
-        awful.key({ mod, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end,
-        {description = "swap with previous client by index", group = "client"}),
-        awful.key({ mod, "Control" }, "j", function () awful.screen.focus_relative( 1) end,
-        {description = "focus the next screen", group = "screen"}),
-        awful.key({ mod, "Control" }, "k", function () awful.screen.focus_relative(-1) end,
-        {description = "focus the previous screen", group = "screen"}),
-        awful.key({ mod,           }, "u", awful.client.urgent.jumpto,
-        {description = "jump to urgent client", group = "client"}),
-        awful.key({ mod,           }, "Tab",
-        function ()
-        awful.client.focus.history.previous()
-        if client.focus then
-        client.focus:raise()
-        end
-        end,
-        {description = "go back", group = "client"}),
+        awful.key(
+            {mod, "Shift"}, "j",
+            function ()
+                awful.client.swap.byidx(1)
+            end,
+            {description = "swap with next client by index", group = "client"}
+        ),
+        awful.key(
+            {mod, "Shift"}, "k",
+            function ()
+                awful.client.swap.byidx(-1)
+            end,
+            {description = "swap with previous client by index", group = "client"}
+        ),
+        awful.key(
+            {mod, "Control"}, "j",
+            function () awful.screen.focus_relative(1)
+            end,
+            {description = "focus the next screen", group = "screen"}
+        ),
+        awful.key(
+            {mod, "Control"}, "k",
+            function ()
+                awful.screen.focus_relative(-1)
+            end,
+            {description = "focus the previous screen", group = "screen"}
+        ),
+        awful.key(
+            {mod}, "u",
+            awful.client.urgent.jumpto,
+            {description = "jump to urgent client", group = "client"}
+        ),
+        awful.key(
+            {mod}, "Tab",
+            function ()
+                awful.client.focus.history.previous()
+                if client.focus then
+                    client.focus:raise()
+                end
+            end,
+            {description = "go back", group = "client"}
+        ),
 
         -- Standard program
         awful.key({ mod,           }, "Return", function () awful.spawn(terminal) end,
