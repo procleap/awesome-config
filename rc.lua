e61e433d5afdc64d6a8cb82c09e8679258c62c37
procleap <procleap@users.noreply.github.com>
2020-05-03 19:52:12 +0200

    Update main menu's text

--- rc.lua
+++ rc.lua
@@ -72,18 +72,18 @@ awful.layout.layouts = {
 -- {{{ Menu
 -- Create a launcher widget and a main menu
 myawesomemenu = {
-   {"hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end},
-   {"manual", terminal .. " -e man awesome" },
-   {"edit config", editor_cmd .. " " .. awesome.conffile},
-   {"restart", awesome.restart},
-   {"quit", function() awesome.uit() end },
+   {"Hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end},
+   {"Manual", terminal .. " -e man awesome" },
+   {"Edit Config", editor_cmd .. " " .. awesome.conffile},
+   {"Restart", awesome.restart},
+   {"Quit", function() awesome.uit() end },
 }
 
 mymainmenu = awful.menu(
     {
         items = {
-            {"awesome", myawesomemenu, beautiful.awesome_icon},
-            {"open terminal", terminal}
+            {"Awesome", myawesomemenu, beautiful.awesome_icon},
+            {"Open Terminal", terminal}
         }
     }
 )
