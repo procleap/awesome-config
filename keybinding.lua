f3f33aa119cb7656f8ca37ab80adcad2721291ec
procleap <procleap@users.noreply.github.com>
2020-05-06 21:09:05 +0200

    Format code

--- keybinding.lua
+++ keybinding.lua
@@ -208,10 +208,10 @@ keys.global = gears.table.join(
             {mod}, "x",
             function ()
                 awful.prompt.run {
-                prompt       = "Run Lua code: ",
-                textbox      = awful.screen.focused().mypromptbox.widget,
-                exe_callback = awful.util.eval,
-                history_path = awful.util.get_cache_dir() .. "/history_eval"
+                    prompt       = "Run Lua code: ",
+                    textbox      = awful.screen.focused().mypromptbox.widget,
+                    exe_callback = awful.util.eval,
+                    history_path = awful.util.get_cache_dir() .. "/history_eval"
                 }
             end,
             {description = "lua execute prompt", group = "awesome"}
