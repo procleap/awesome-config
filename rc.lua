aca56e4ce3a054221f697a23b35b12b3d53fbf39
procleap <procleap@users.noreply.github.com>
2020-05-06 10:09:30 +0200

    Set newly created windows to slave

--- rc.lua
+++ rc.lua
@@ -435,8 +435,10 @@ awful.rules.rules = {
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function (c)
     -- Set the windows at the slave,
-    -- i.e. put it at the end of others instead of setting it master.
-    -- if not awesome.startup then awful.client.setslave(c) end
+    -- i.e. put it at the end of others instead of setting it as master.
+    if not awesome.startup then
+        awful.client.setslave(c)
+    end
 
     if awesome.startup
       and not c.size_hints.user_position
