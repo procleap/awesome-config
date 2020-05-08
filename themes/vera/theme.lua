3d39946dec9c2e57b09730111baea31e37ac849c
procleap <procleap@users.noreply.github.com>
2020-05-08 20:06:53 +0200

    Highlight selected tag

--- themes/vera/theme.lua
+++ themes/vera/theme.lua
@@ -49,6 +49,8 @@ theme.useless_gap    = dpi(6)
 -- Taglist
 --
 
+theme.taglist_bg_focus = "#ff0000"
+
 -- Generate taglist squares:
 local taglist_square_size = dpi(4)
 theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
