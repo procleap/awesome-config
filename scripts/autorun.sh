f8e2f0b735a21b3c082bdc748595e54a5fdb9e69
procleap <procleap@users.noreply.github.com>
2020-05-11 08:59:58 +0200

    Amend comment

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-# Check if process is already running before attempting to start it.
+# Check if process is already running before attempting to start it
 function run {
   if ! pgrep -f $1 ; then
     $@&
   fi
 }
 
-# Load previously configured wallpaper.
+# Load previously configured wallpaper
 run nitrogen --restore
 
-# Enable compositor effects.
+# Enable compositor effects
 run picom --config ~/.config/awesome/conf/picom.cfg
 
-# Network manager tray icon.
+# Network manager tray icon
 run nm-applet
 
-# Start power management.
+# Start power management
 run xfce4-power-manager
 
 # Start screen locker
