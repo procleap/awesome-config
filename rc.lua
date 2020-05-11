519cc59cb1107f8e62e7af88388ef79a4adf6aa9
procleap <procleap@users.noreply.github.com>
2020-05-11 19:32:49 +0200

    Remove luarocks loader

--- rc.lua
+++ rc.lua
@@ -3,10 +3,6 @@
 -- https://github.com/procleap/awesome-config
 --
 
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
-pcall(require, "luarocks.loader")
-
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
