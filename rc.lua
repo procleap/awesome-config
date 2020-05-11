e7931616759262e46fa665a77a38bf46a29030db
procleap <procleap@users.noreply.github.com>
2020-05-11 19:32:10 +0200

    Add header comment

--- rc.lua
+++ rc.lua
@@ -1,3 +1,8 @@
+--
+-- My Awesome config
+-- https://github.com/procleap/awesome-config
+--
+
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
